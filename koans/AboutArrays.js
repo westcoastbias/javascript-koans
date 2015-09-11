dea31142514d198a595a762cbf3d4826487fee39
Lucas Ruprecht <lruprecht@gmail.com>
2015-09-10 23:30:35 -0700

    Sixth change to AboutArrays - attempted fix for change in assigned array

--- koans/AboutArrays.js
+++ koans/AboutArrays.js
@@ -66,7 +66,7 @@ describe("About Arrays", function() {
 
     var assignedArray = array;
     assignedArray[5] = "changed in assignedArray";
-    expect(array[5]).toBe("five");
+    expect(array[5]).toBe("changed in assignedArray");
 
     var copyOfArray = array.slice();
     copyOfArray[3] = "changed in copyOfArray";
